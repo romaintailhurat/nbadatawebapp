1338f01ab8e5149875b3ee2beaa5f504094ee8e8
Romain Tailhurat <romain.tailhurat@gmail.com>
2023-01-03 11:07:14 +0000

    Player page, displaying name

--- pages/players/[playerid].jsx
+++ pages/players/[playerid].jsx
@@ -10,62 +10,64 @@ import { getSupabaseClient } from '../../remote/supabaseclient.js';
 const supabase = getSupabaseClient();
 
 function getColorFromScore(score) {
-  
+
   switch (true) { // weird, see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
-		case score < 0:
-			return blue[500];      
-    case score >= 0 && score < 10:      
+    case score < 0:
+      return blue[500];
+    case score >= 0 && score < 10:
       return lightBlue[500];
     case score >= 10 && score < 20:
       return lightGreen[500];
     case score >= 20 && score < 30:
       return orange[500];
     case score >= 30:
-      return red[200]  ;
-		default:
-			return grey[300];
-	}
+      return red[200];
+    default:
+      return grey[300];
+  }
 }
 
-function Player({ perfs }) {
-	const router = useRouter();
-	const { playerid } = router.query;
-	return (
-		<div>
-			<h1>This is the page for {playerid}</h1>
-			{perfs.map(perf => (
-				<div key={perf.date}>
-					<Card>
-						<CardHeader />
-						<CardContent>
-							<Typography
-								sx={{ fontSize: 14 }}
-								color="text.secondary"
-								gutterBottom
-							>
-								{perf.date}
-							</Typography>
-							<Typography sx={{ color: getColorFromScore(perf.score) }} variant="h2">
-								{perf.score}
-							</Typography>
-						</CardContent>
-					</Card>
-				</div>
-			))}
-		</div>
-	);
+function Player({ perfs, name }) {
+  const router = useRouter();
+  const { playerid } = router.query;
+  return (
+    <div>
+      <h1>This is the page for {name} {playerid}</h1>
+      {perfs.map(perf => (
+        <div key={perf.date}>
+          <Card>
+            <CardHeader />
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                {perf.date}
+              </Typography>
+              <Typography sx={{ color: getColorFromScore(perf.score) }} variant="h2">
+                {perf.score}
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
-	const { playerid } = context.query;
+  const { playerid } = context.query;
 
-	let { data: perfs, error } = await supabase
-		.from('perfs') // join with the players table to get the name
-		.select('score, date')
-		.order('date', { ascending: false })
-		.eq('player_id', playerid);
+  let { data: perfs, error } = await supabase
+    .from('perfs') // join with the players table to get the name
+    .select('score, date')
+    .order('date', { ascending: false })
+    .eq('player_id', playerid);
 
-	return { props: { perfs } };
+  let name = await supabase.from('players').select('player_name').eq('player_id', playerid);
+
+  return { props: { perfs, name: name } };
 }
 
 export default Player;

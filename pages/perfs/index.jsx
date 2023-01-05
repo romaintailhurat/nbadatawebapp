a7e18ed45a8f8f7ca55235046a5e0ce1c44e1dfa
Romain Tailhurat <romain.tailhurat@gmail.com>
2023-01-05 06:58:46 +0000

    Perfs page, typo fix

--- pages/perfs/index.jsx
+++ pages/perfs/index.jsx
@@ -2,26 +2,26 @@ import { getSupabaseClient } from '../../remote/supabaseclient.js';
 
 const supabase = getSupabaseClient();
 
-function Perfs({perfs}) {
-  return(
+function Perfs({ perfs }) {
+  return (
     <div>
       <h1>Perfs</h1>
       <h2>Last perfs</h2>
-      {perfs.map(perf => <p key={player_id}>{perf.player_id}: {perf.score}</p>)}
+      {perfs.map(perf => <p key={perf.player_id}>{perf.player_id}: {perf.score}</p>)}
     </div>
   )
 }
 
 export async function getServerSideProps(context) {
-	let { data: perfs, error } = await supabase
-		.from('perfs')
-		.select('player_id, score')
-		.order('score', { ascending: false })
+  let { data: perfs, error } = await supabase
+    .from('perfs')
+    .select('player_id, score')
+    .order('score', { ascending: false })
     .eq('date', '2022-01-07');
 
-	return {
-		props: { perfs: perfs } // will be passed to the page component as props
-	};
+  return {
+    props: { perfs: perfs } // will be passed to the page component as props
+  };
 }
 
 export default Perfs;
